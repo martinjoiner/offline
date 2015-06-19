6fd9bc4e601234b6c7df47235c5378d891af289d
Martin Joiner <martin@martinjoiner.co.uk>
2015-06-19 13:40:39 +0100

    Initial commit

---
+++ public_html/GET/latest/index.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Content-Type: application/json');
+
+
+$skvReturn = array();
+
+$skvReturn['appVersion'] = '0.1';
+
+// Current date in the format "Fri Jun 19 2015 11:26:49 BST 2015"
+$skvReturn['arrDate'] = array();
+$skvReturn['arrDate'][] = intval( date("Y") ); // Year
+$skvReturn['arrDate'][] = intval( date("m") ); // month
+$skvReturn['arrDate'][] = intval( date("j") ); // day 
+$skvReturn['arrDate'][] = intval( date("G") ); // hour 
+$skvReturn['arrDate'][] = intval( date("i") ); // minute 
+$skvReturn['arrDate'][] = intval( date("s") ); // second 
+
+$skvReturn['arrChapters'] = array();
+
+$iLimit = rand( 9, 24 );
+for( $i = 0; $i < $iLimit; $i++ ){
+	$skvReturn['arrChapters'][$i] = md5( $i . time() . $i );
+}
+
+echo json_encode( $skvReturn );
+
+?>
---
+++ public_html/js/offline.js
@@ -0,0 +1,63 @@
+
+
+$.ajax({
+	type: "GET",
+	url: "/GET/latest/index.php", 
+	dataType: "json"
+}).done(function( data ){
+
+	//displayDate( data.arrDate );
+	//displayChapters( data.arrChapters );
+
+	// AJAX succeeded, lets pass the data to the display functions 
+	localforage.setItem('arrDate', data.arrDate, function(err, value){ displayDate(value) } );
+	localforage.setItem('arrChapters', data.arrChapters, function(err, value){ displayChapters(value) } );
+
+}).fail( function(){
+
+	// AJAX failed, lets grab data from local cache and call the display functions 
+	localforage.getItem('arrDate').then( displayDate );
+	localforage.getItem('arrChapters').then( displayChapters );
+
+});
+
+
+function displayData( data ){
+
+}
+
+function displayDate( arrDate ){
+	var objDate = new Date( arrDate[0], arrDate[1], arrDate[2], arrDate[3], arrDate[4], arrDate[5] );
+	var strDate = 'Loaded data from ' + objDate;
+
+	$('#content').append( '<h2>' + strDate + '</h2>' );
+}
+
+
+function displayChapters( arrChapters ){
+
+	var strHTML = '<ul>';
+	for( var i = 0, iLimit = arrChapters.length; i < iLimit; i++ ){
+		strHTML += '<li>' + arrChapters[i] + '</li>';
+	}
+	strHTML += '</ul>';
+	$('#content').append( strHTML );
+}
+
+
+
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+	window.applicationCache.addEventListener('updateready', function(e) {
+		if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+			// Browser downloaded a new app cache.
+			if (confirm('A new version of this site is available. Load it?')) {
+				window.location.reload();
+			}
+		} else {
+			// Manifest didn't changed. Nothing new to server.
+		}
+	}, false);
+
+}, false);
